1e93a1b5a2e9b30e288c8e34e55a3223120fa5b6
dontknowbutfeelsok <94104801+dontknowbutfeelsok@users.noreply.github.com>
2021-11-17 11:45:49 +0530

    added a comment explaining; while loop.
    read the comment.

--- luhn.py
+++ luhn.py
@@ -1,8 +1,5 @@
 #credit card verifier
 
-#from random import randint
-#import re
-
 i = 1
 b = []
 c = []
@@ -13,7 +10,6 @@ d = []
 test = 79927398713
 
 testarr = [int(x) for x in str(test)]
-
 
 while i != len(str(test)) or i != len(str(test)) + 1:
     try:
@@ -26,15 +22,26 @@ for i in b:
     c.append(i*2)
     i+=1
 
-for w in c:
-    if len(c[w]) > 0:
+'''
+first, you bring all the >1   numbers in one list by the while loop.
+then you convert the list to a string by ''.join(map()) func{say y}; also
+make a list of the left numbers {say g} in the variable c. then separate
+every thing from y and create a list of that. then g + y. done. good job. 
+'''
+
+cisstr = ''.join(map(str, c))
+
+print(cisstr)
+
+w = 0
+
+while w < len(cisstr):
+    if len(''.join(map(str, c[w]))):
         d.append(c[w])
         print(d)
         w+=1
+    else:
+        continue
 
-print('c =', c)
-
-
-
-    
-
+#print('c =', c)
+#print('d =', d)
