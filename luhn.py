b9aa5dd78fa0689e9817d2617ad63bf13a28bca2
dontknowbutfeelsok <94104801+dontknowbutfeelsok@users.noreply.github.com>
2021-11-20 13:09:42 +0530

    creating 2 files; this is the first file

--- luhn.py
+++ luhn.py
@@ -1,75 +1,30 @@
-#credit card verifier
+#credit card verifier/generator
 import random
 import time
-
+import isValid
 
 a = [int(i) for i in range(0,10)]
 creditcardno = []
 
-boolean = False
-
-def isValidCredit(number):
-
-    i = 1
-    n = 0
-    testarr_second_int = []
-    c = []
-    f = []    
-
-    testarr = [int(x) for x in str(number)]
-
-    while i != len(str(number)) or i != len(str(number)) + 1:
-        try:
-            testarr_second_int.append(testarr[i])
-            i += 2
-        except IndexError:
-            break
-
-    while n != len(str(number)) or n != len(str(number)) + 1:
-        try:
-            f.append(testarr[n])
-            n += 2
-        except IndexError:
-            break
-
-    for i in testarr_second_int:
-        c.append(i*2)
-        i+=1
-
-    #c to str and then sum
-
-    cinstr = ''.join(map(str, c))
-
-    cinstr_sum = sum(map(int, cinstr))
-
-    #m = [k for k in testarr if k not in testarr_second_int]
-
-    #f to str and then sum
-
-    finstr = ''.join(map(str, f))
-
-    finstr_sum = sum(map(int, finstr))
-
-    whole_sum = finstr_sum + cinstr_sum
-
-    if whole_sum % 10 == 0:
-        boolean = True
-    else:
-        boolean = False
-
-while boolean == False:
+while isValid.boolean == False:
     raw_i = 0
     while raw_i < 16:
         creditcardno.append(random.choices(a))    
         raw_i+=1
+    if len(creditcardno) > 16:
+        creditcardno.clear()
     #it creates a flatten list out of a list of lists
     creditcardno_flatlist = sum(creditcardno, [])
 
     #converts list to int
-    creditcardno_real = int(''.join(map(str, creditcardno_flatlist)))
+    creditcard = ''.join(map(str, creditcardno_flatlist))
 
-    print(creditcardno_real, '\n')
+    creditcard.strip()
+
+    print(creditcardno_flatlist)
+
+    print('checking ', creditcard, '\n')
 
     time.sleep(0.2)
 
-    isValidCredit(creditcardno_real)
+    isValidCredit(int(creditcard))
